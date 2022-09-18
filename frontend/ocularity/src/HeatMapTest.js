4db55cb7b059b7fa3e4ddb6bec8e25d0163cbb3c
ViridianCitrus <kevin.xiaorui.wang@gmail.com>
2022-09-17 21:31:07 -0400

    adding flask backend
    add flask backend, server call in heatmap

--- frontend/ocularity/src/HeatMapTest.js
+++ frontend/ocularity/src/HeatMapTest.js
@@ -1,23 +1,24 @@
 import HeatMap from "react-heatmap-grid";
-
+import React, { useState, useEffect } from 'react';
 
 
 
 function HeatMapT() {
+  const [testData, setTestData] = useState([[],[]]);
   const xLabels = new Array(4).fill(" ");
-  const yLabels = new Array(3).fill(" ");
-  const data = new Array(yLabels.length)
-    .fill(0)
-    .map(() =>
-      new Array(xLabels.length).fill(0).map(() => Math.floor(Math.random() * 100))
-  );
+  const yLabels = new Array(2).fill(" ");
 
-  \
+  useEffect(() => {
+    fetch('http://localhost:3005/getData')
+      .then((response) => response.json())
+      .then((data) => setTestData(data.data));
+  }, [setTestData]);
+
 
   return (
     <div>
-      <HeatMap xLabels={xLabels} yLabels={yLabels} data={data}
-        yLabelWidth={0} yLabelTextAlign="center" height={150} xLabelsVisibility={[false]} background="#C23A22"/>
+      <HeatMap xLabels={xLabels} yLabels={yLabels} data={testData}
+        yLabelWidth={0} yLabelTextAlign="center" height={150} xLabelsVisibility={[false]} background="#C23A22" />
     </div>
   );
 }
